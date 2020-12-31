16a4a43f311a8c734ea53e4ddba13fc876b37769
jontran1 <jtran001@citymail.cuny.edu>
2020-12-30 22:56:07 -0500

    Add paging. Users must load in via pages.

--- DatingApp.API/Controllers/UsersController.cs
+++ DatingApp.API/Controllers/UsersController.cs
@@ -26,12 +26,16 @@ namespace DatingApp.API.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetUsers()
+        public async Task<IActionResult> GetUsers([FromQuery]UserParams userParams)
         {
-            var users = await _repo.GetUsers();
+            var users = await _repo.GetUsers(userParams);
 
             var userToReturn = _mapper.Map<IEnumerable<UserForListDto>>(users);
 
+            // Let the client side know its a pagenation.
+            Response.AddPagination(users.CurrentPage, users.PageSize, 
+                users.TotalCount, users.TotalPages);
+            
             return Ok(userToReturn);
         }
 
