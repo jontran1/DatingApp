fd64463ce129842a0b3d83cebee2a93880ffcfcd
jontran1 <jtran001@citymail.cuny.edu>
2021-01-24 00:40:58 -0500

    Add SPA frontend likes functionaility.

--- DatingApp-SPA/src/app/members/memeber-card/memeber-card.component.ts
+++ DatingApp-SPA/src/app/members/memeber-card/memeber-card.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { User } from 'src/app/_models/user';
+import { AlertifyService } from 'src/app/_services/alertify.service';
+import { AuthService } from 'src/app/_services/auth.service';
+import { UserService } from 'src/app/_services/user.service';
 
 /**
  * A child component of memeber-list component. 
@@ -13,9 +16,18 @@ export class MemeberCardComponent implements OnInit {
 
   @Input() user: User;
   
-  constructor() { }
+  constructor(private authService: AuthService, private userService: UserService,
+    private alertify: AlertifyService) { }
 
   ngOnInit() {
+  }
+
+  sendLike(id: Number){
+    this.userService.sendLike(this.authService.decodedToken.nameid, id).subscribe(data => {
+      this.alertify.success('You have like: ' + this.user.knownAs);
+    }, error => {
+      this.alertify.error(error);
+    })
   }
 
 }
